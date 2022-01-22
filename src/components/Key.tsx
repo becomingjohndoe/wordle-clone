fde3fb086d397483f7aa60bfcbae4522101ffa3f
becomingjohndoe <karl.swatman@gmail.com>
2022-01-22 01:11:57 +0000

    updates key styling with icons

--- src/components/Key.tsx
+++ src/components/Key.tsx
@@ -1,17 +1,27 @@
+import { AiOutlineEnter } from "react-icons/ai";
+import { BsBackspace } from "react-icons/bs";
+
 interface KeyProps {
 	value: string;
 	onClick: (e: React.MouseEvent<Element>) => void;
+	status: (letter: string) => string;
 }
 
-export const Key = ({ value, onClick }: KeyProps) => {
+export const Key = ({ value, onClick, status }: KeyProps) => {
 	return (
 		<div
-			className="key"
+			className={`key ${status(value)}`}
 			onClick={(e) => {
 				if (e) onClick(e);
 			}}
 		>
-			{value}
+			{value === "DELETE" ? (
+				<BsBackspace />
+			) : value === "ENTER" ? (
+				<AiOutlineEnter />
+			) : (
+				value
+			)}
 		</div>
 	);
 };
