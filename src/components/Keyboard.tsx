8d3490fd8fc22ae718eb3c12f7f157eb19f009f2
becomingjohndoe <karl.swatman@gmail.com>
2022-01-22 01:12:45 +0000

    adds check status functionality to display correct guesses on keyboard

--- src/components/Keyboard.tsx
+++ src/components/Keyboard.tsx
@@ -1,11 +1,15 @@
 import { Key } from "./Key";
 import React from "react";
+import { checkGuess } from "./Row";
 type Props = {
 	setGuesses: (guesses: string[][]) => void;
 	guesses: string[][];
 	tick: number;
 	checkWin: () => boolean;
 	setTick: (tick: number) => void;
+	setEnterPressed: (enterPressed: boolean[]) => void;
+	enterPressed: boolean[];
+	word: string[];
 };
 
 export const Keyboard = ({
@@ -14,6 +18,9 @@ export const Keyboard = ({
 	guesses,
 	checkWin,
 	setTick,
+	setEnterPressed,
+	enterPressed,
+	word,
 }: Props) => {
 	const handleClick = (e: React.MouseEvent<Element>) => {
 		if (e) {
@@ -25,46 +32,78 @@ export const Keyboard = ({
 	};
 
 	const handleEnter = () => {
+		const newEnterPressed: boolean[] = [...enterPressed];
+		newEnterPressed[tick] = true;
+		setEnterPressed(newEnterPressed);
 		if (checkWin()) alert("You Win!");
-		else setTick(tick + 1);
+		else {
+			setTick(tick + 1);
+		}
+	};
+
+	const handleDelete = () => {
+		const newguesses: string[][] = [...guesses];
+		const turn: number = newguesses[tick].findIndex((e) => e === "");
+		turn === -1 ? (newguesses[tick][4] = "") : (newguesses[tick][turn - 1] = "");
+		setGuesses(newguesses);
+	};
+
+	const checkStatus = (letter: string): string => {
+		let includedInGuess = guesses.some((e) => e.indexOf(letter) !== -1);
+		let correctGuess = guesses.some(
+			(e) => e.indexOf(letter) !== -1 && e.indexOf(letter) === word.indexOf(letter)
+		);
+		let indexInGuesses = 0;
+		guesses.forEach((e, i) => {
+			if (e.indexOf(letter) !== -1) {
+				indexInGuesses = i;
+			}
+		});
+		if (enterPressed[indexInGuesses]) {
+			if (correctGuess) {
+				return "correct";
+			} else if (includedInGuess && word.includes(letter)) return "includes";
+			else if (includedInGuess) return "incorrect";
+		}
+		return "";
 	};
 
 	return (
 		<div>
 			<div className="keyboard-row">
-				<Key value="Q" onClick={handleClick} />
-				<Key onClick={handleClick} value="W" />
-				<Key onClick={handleClick} value="E" />
-				<Key onClick={handleClick} value="R" />
-				<Key onClick={handleClick} value="T" />
-				<Key onClick={handleClick} value="Y" />
-				<Key onClick={handleClick} value="U" />
-				<Key onClick={handleClick} value="I" />
-				<Key onClick={handleClick} value="O" />
-				<Key onClick={handleClick} value="P" />
+				<Key value="Q" onClick={handleClick} status={checkStatus} />
+				<Key onClick={handleClick} value="W" status={checkStatus} />
+				<Key onClick={handleClick} value="E" status={checkStatus} />
+				<Key onClick={handleClick} value="R" status={checkStatus} />
+				<Key onClick={handleClick} value="T" status={checkStatus} />
+				<Key onClick={handleClick} value="Y" status={checkStatus} />
+				<Key onClick={handleClick} value="U" status={checkStatus} />
+				<Key onClick={handleClick} value="I" status={checkStatus} />
+				<Key onClick={handleClick} value="O" status={checkStatus} />
+				<Key onClick={handleClick} value="P" status={checkStatus} />
 			</div>
 			<div className="keyboard-row">
-				<Key onClick={handleClick} value="A" />
-				<Key onClick={handleClick} value="S" />
-				<Key onClick={handleClick} value="D" />
-				<Key onClick={handleClick} value="F" />
-				<Key onClick={handleClick} value="G" />
-				<Key onClick={handleClick} value="H" />
-				<Key onClick={handleClick} value="J" />
-				<Key onClick={handleClick} value="K" />
-				<Key onClick={handleClick} value="L" />
+				<Key onClick={handleClick} value="A" status={checkStatus} />
+				<Key onClick={handleClick} value="S" status={checkStatus} />
+				<Key onClick={handleClick} value="D" status={checkStatus} />
+				<Key onClick={handleClick} value="F" status={checkStatus} />
+				<Key onClick={handleClick} value="G" status={checkStatus} />
+				<Key onClick={handleClick} value="H" status={checkStatus} />
+				<Key onClick={handleClick} value="J" status={checkStatus} />
+				<Key onClick={handleClick} value="K" status={checkStatus} />
+				<Key onClick={handleClick} value="L" status={checkStatus} />
 			</div>
 			<div className="keyboard-row">
-				<Key onClick={handleEnter} value="ENTER" />
+				<Key onClick={handleEnter} value="ENTER" status={checkStatus} />
 
-				<Key onClick={handleClick} value="Z" />
-				<Key onClick={handleClick} value="X" />
-				<Key onClick={handleClick} value="C" />
-				<Key onClick={handleClick} value="V" />
-				<Key onClick={handleClick} value="B" />
-				<Key onClick={handleClick} value="N" />
-				<Key onClick={handleClick} value="M" />
-				<Key onClick={handleClick} value="DELETE" />
+				<Key onClick={handleClick} value="Z" status={checkStatus} />
+				<Key onClick={handleClick} value="X" status={checkStatus} />
+				<Key onClick={handleClick} value="C" status={checkStatus} />
+				<Key onClick={handleClick} value="V" status={checkStatus} />
+				<Key onClick={handleClick} value="B" status={checkStatus} />
+				<Key onClick={handleClick} value="N" status={checkStatus} />
+				<Key onClick={handleClick} value="M" status={checkStatus} />
+				<Key onClick={handleDelete} value="DELETE" status={checkStatus} />
 			</div>
 		</div>
 	);
--- src/components/Row.tsx
+++ src/components/Row.tsx
@@ -3,28 +3,61 @@ import { Cell } from "./Cell";
 type Props = {
 	guesses: string[][];
 	word: string[];
+	enterPressed: boolean[];
+	keyboardGuesses: {
+		[key: string]: string;
+	};
+	setKeyboardGuesses: (keyboardGuesses: { [key: string]: string }) => void;
 };
 
-export const Row = ({ guesses, word }: Props) => {
-	// check if guess was correct
-	const checkGuess = (
-		letter: string,
-		row: number,
-		guessIndex: number
-	): string => {
-		if (!guesses[guessIndex].includes("")) {
-			if (letter === word[row]) return "correct";
-			else if (word.includes(letter)) return "includes";
+export const checkGuess = (
+	letter: string,
+	row: number,
+	guessIndex: number,
+	guesses: string[][],
+	word: string[],
+	enterPressed: boolean[],
+	setKeyboardGuesses: (keyboardGuesses: { [key: string]: string }) => void,
+	keyboardGuesses: { [key: string]: string }
+): string => {
+	const newKeyboardGuesses = { ...keyboardGuesses };
+
+	if (!guesses[guessIndex].includes("") && enterPressed[guessIndex]) {
+		if (letter === word[row]) {
+			return "correct";
+		} else if (word.includes(letter)) {
+			return "includes";
 		}
-		return "incorrect";
-	};
+	}
+
+	return "incorrect";
+};
+
+export const Row = ({
+	guesses,
+	word,
+	enterPressed,
+	setKeyboardGuesses,
+	keyboardGuesses,
+}: Props) => {
+	// check if guess was correct
+
 	return (
 		<div className="row">
 			{guesses.map((guess, guessIndex) => {
 				return guess.map((letter, row) => {
 					return (
 						<Cell
-							state={checkGuess(letter, row, guessIndex)}
+							state={checkGuess(
+								letter,
+								row,
+								guessIndex,
+								guesses,
+								word,
+								enterPressed,
+								setKeyboardGuesses,
+								keyboardGuesses
+							)}
 							value={letter}
 							key={row}
 						/>
